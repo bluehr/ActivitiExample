309abb81ee64ccf59d282e4257c7c9abd520c0f3
fys2000 <fys2000@dzkf.com>
2018-12-14 10:56:00 +0800

    ActivitiRule测试

--- ActivitiSpring/src/test/java/cn/bluehr/ActivitiEngineTest.java
+++ ActivitiSpring/src/test/java/cn/bluehr/ActivitiEngineTest.java
@@ -3,14 +3,18 @@ package cn.bluehr;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.test.ActivitiRule;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.InputStream;
+import java.util.Map;
 
 /**
  * @Auther: fys2000
@@ -19,6 +23,10 @@ import java.io.InputStream;
  */
 
 public class ActivitiEngineTest {
+
+    @Rule
+    public ActivitiRule activitiRule  =  new ActivitiRule();
+
     @Test
     public void testEngine() {
         ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "activiti.cfg.xml" });
@@ -55,6 +63,16 @@ public class ActivitiEngineTest {
     public void getDefaultProcessEngine(){
         ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
         Assert.assertNotNull(engine);
+    }
+
+    @Test
+    public void activitiUnitTest(){
+        ProcessEngine engine = activitiRule.getProcessEngine();
+        System.out.println(engine.getName());
+        Map<String, String> properties = engine.getManagementService().getProperties();
+        for (String s : properties.keySet()) {
+            System.out.println(s+":"+properties.get(s));
+        }
     }
 
 
