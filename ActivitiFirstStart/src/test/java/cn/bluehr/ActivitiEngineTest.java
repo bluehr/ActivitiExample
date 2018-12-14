468757814dca9a357ea0a17c59dc5b61a1d4428a
fys2000 <fys2000@dzkf.com>
2018-12-14 15:43:28 +0800

    移除Spring模块

--- ActivitiFirstStart/src/test/java/cn/bluehr/ActivitiEngineTest.java
+++ ActivitiFirstStart/src/test/java/cn/bluehr/ActivitiEngineTest.java
@@ -3,15 +3,12 @@ package cn.bluehr;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.ProcessEngines;
-import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.test.ActivitiRule;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.InputStream;
 import java.util.Map;
@@ -27,18 +24,18 @@ public class ActivitiEngineTest {
     @Rule
     public ActivitiRule activitiRule  =  new ActivitiRule();
 
-    @Test
-    public void testEngine() {
-        /*使用Spring来构建这个类 但是真正和Spring整合配置的时候我们还会更改一下配置文件。*/
-        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "activiti.cfg.xml" });
-        ProcessEngineConfiguration processEngineConfiguration = (ProcessEngineConfiguration) context.getBean("processEngineConfiguration");
-        ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
-        Assert.assertEquals("MyProcessEngine", processEngine.getName());
-        RuntimeService runtimeService = processEngine.getRuntimeService();
-        Assert.assertNotNull(runtimeService);
-        TaskService taskService = processEngine.getTaskService();
-        Assert.assertNotNull(taskService);
-    }
+//    @Test
+//    public void testEngine() {
+//        /*使用Spring来构建这个类 但是真正和Spring整合配置的时候我们还会更改一下配置文件。*/
+//        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "activiti.cfg.xml" });
+//        ProcessEngineConfiguration processEngineConfiguration = (ProcessEngineConfiguration) context.getBean("processEngineConfiguration");
+//        ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
+//        Assert.assertEquals("MyProcessEngine", processEngine.getName());
+//        RuntimeService runtimeService = processEngine.getRuntimeService();
+//        Assert.assertNotNull(runtimeService);
+//        TaskService taskService = processEngine.getTaskService();
+//        Assert.assertNotNull(taskService);
+//    }
 
     @Test
     public void testDefaltEngine(){
